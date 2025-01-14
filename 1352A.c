5a7d8cbe2874a1a84d085a070e836cb425e97d98
silent-064 <prappoprotic@gmail.com>
2025-01-14 21:04:02 +0600

    Sum of Round Numbers

---
+++ 1352A.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a, x, i;
+    scanf("%d", &a);  
+    while (a--) {
+        scanf("%d", &x);  
+        int power = 1;
+        int round_value[5];  
+        int count = 0;
+
+        
+        while (x > 0) {
+            int digit = x % 10;
+            if (digit > 0) {
+                round_value[count++] = digit * power;  
+            }
+            x /= 10;
+            power *= 10;  
+        }
+
+       
+        printf("%d\n", count);
+        for (i = 0; i < count; i++) {
+            printf("%d ", round_value[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
