f0cf4b4d5786c852c2fc865ad357b3dc9727c46d
silent-064 <prappoprotic@gmail.com>
2025-01-15 12:31:05 +0600

    Tram

---
+++ 116A.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a,x,i,y;
+int current_passengers=0,max_passenger=0;
+scanf("%d",&a);
+for(i=0; i<a; i++)
+{
+
+scanf("%d %d" ,&x ,&y);
+current_passengers-=x;
+current_passengers+=y;
+if(current_passengers > max_passenger)
+{
+    max_passenger=current_passengers;
+
+}
+
+}
+printf("%d",max_passenger);
+
+
+
+
+
+    return 0;
+}
--- 1399A.c
+++ 1399A.c
@@ -1,44 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);  
+}
 
 int main() {
     int t;
-    scanf("%d", &t);  // টেস্ট কেস সংখ্যা
-
+    scanf("%d", &t);  
     while (t--) {
-        int n, i, j;
+        int n, i;
         scanf("%d", &n);
         int a[n];
-
         for (i = 0; i < n; i++) {
-            scanf("%d", &a[i]);  // অ্যারের ইনপুট
+            scanf("%d", &a[i]);  
         }
 
-        // সর্টিং (বাবল সর্ট)
-        for (i = 0; i < n - 1; i++) {
-            for (j = 0; j < n - i - 1; j++) {
-                if (a[j] > a[j + 1]) {
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                }
-            }
-        }
+        qsort(a, n, sizeof(int), compare);  
 
-        // শর্ত চেক করা
-        int possible = 1;  // ধরে নিচ্ছি এটি সম্ভব
+        int possible = 1;  
         for (i = 1; i < n; i++) {
-            if (a[i] - a[i - 1] > 1) {  // পার্থক্য ১-এর বেশি হলে
+            if (a[i] - a[i - 1] > 1) {  
                 possible = 0;
                 break;
             }
         }
 
-        // আউটপুট প্রিন্ট
-        if (possible) 
-            printf("YES\n");
-        else 
-            printf("NO\n");
+        if (possible) printf("YES\n");
+        else printf("NO\n");
     }
-
     return 0;
 }
