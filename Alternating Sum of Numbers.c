921e26f9a74ec47ec0056176ec3fb297e6856277
silent-064 <prappoprotic@gmail.com>
2025-02-23 02:27:41 +0600

    Div3

---
+++ Alternating Sum of Numbers.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d" ,&a);
+while(a--)
+{
+    int b;
+    scanf("%d" ,&b);
+    int array[b+1];
+    int i;
+    int sum=0;
+    for(i=0; i<b; i++)
+    {
+     scanf("%d" ,&array[i]);
+    }
+    for(i=0; i<b; i++)
+    {
+        if(i%2==0)
+        {
+          sum+=array[i];
+        }
+        else
+        {
+            sum-=array[i];
+        }
+    }
+    printf("%d" ,sum);
+}
+
+
+
+
+
+
+    return 0;
+}
