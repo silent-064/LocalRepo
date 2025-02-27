afe5510492d3c317fe0c54b0bdd336dfe51f5004
silent-064 <prappoprotic@gmail.com>
2025-02-27 15:03:17 +0600

    cpp

---
+++ learnc++.cpp
@@ -0,0 +1,13 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{string s;
+    getline(cin,s);
+    cout<<s<<"\n";
+    reverse(s.begin(),s.end());
+    cout<<s;
+
+
+
+    return 0;
+}
