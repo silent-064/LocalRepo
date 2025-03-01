bec971560552a9cf994370b35b1b0b7b3be7d856
silent-064 <prappoprotic@gmail.com>
2025-03-01 21:52:56 +0600

    convert first string to int

---
+++ firstdigit.cpp
@@ -0,0 +1,22 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+int a;
+string s;
+cin>>s;
+a=s[0]-'0'; //using ascii '0'---48 '1'---49  '2'----50
+if(a%2==0)
+{
+    cout<<"EVEN";
+}
+else{
+    cout<<"ODD";
+}
+
+
+
+
+
+    return 0;
+}
