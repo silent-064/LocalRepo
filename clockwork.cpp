01e60caec4a8ae1a011b12c1bf867afaca6e5298
silent-064 <prappoprotic@gmail.com>
2025-03-07 21:38:18 +0600

    retry again 900ratings

--- clockwork.cpp
+++ clockwork.cpp
@@ -1,27 +1,41 @@
-#include <iostream>
+/*
+ * Author : Protic Prappo Durjoy
+ * Date : 3/7/25
+ */
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int T;
-    cin >> T; // Number of test cases
-
-    while (T--) {
-        int n;
-        cin >> n;
-        bool possible = false;
-
-        for (int i = 0; i < n; i++) {
-            int t;
-            cin >> t;
-            
-            // Checking if the condition is met
-            if (t <= i * 2 || t <= (n - i - 1) * 2) {
-                possible = true;
-            }
-        }
-
-        cout << (possible ? "NO\n" : "YES\n");
+int main()
+{
+ios::sync_with_stdio(false),cin.tie(0);
+int a;
+cin>>a;
+while(a--)
+{
+int b,c,i;
+cin>>b;
+bool count=false;
+for(i=0; i<b; i++)
+{
+    cin>>c;
+    if(c<=i*2 || c<=(b-i-1)*2)
+    {
+        count=true;
     }
-    
+}
+if(count)
+{
+    cout<<"NO\n";
+}
+else
+cout<<"YES\n";
+
+
+}
+
+
+
+
+
+
     return 0;
 }
