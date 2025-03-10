0ff8c9bf0b688a1fc8ae270f931bac3646c67584
silent-064 <prappoprotic@gmail.com>
2025-03-10 22:11:52 +0600

    easyy

---
+++ new.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date : 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false),cin.tie(0);
+int a;
+cin>>a;
+while(a--)
+{
+cout<<"Hello World\n";
+
+
+
+
+}
+
+
+
+
+
+
+    return 0;
+}
