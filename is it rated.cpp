b4882f3ce1b7af956b0dc8819b83b82cd777d76a
silent-064 <prappoprotic@gmail.com>
2025-03-17 01:05:06 +0600

    900

---
+++ is it rated.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date : 17/3/25
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false),cin.tie(0);
+string a;
+while (getline(cin, a)) { 
+    cout << "no" << endl;
+}
+
+
+
+
+
+
+    return 0;
+}
