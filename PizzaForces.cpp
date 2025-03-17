2dac4ebbf442a8cae41b36fcfe6b319bc2a040b2
silent-064 <prappoprotic@gmail.com>
2025-03-17 19:21:55 +0600

    900 Ratings

---
+++ PizzaForces.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date : 17/3/25
+ * 15-6/20-8/25-10
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false),cin.tie(0);
+int a;
+cin>>a;
+while(a--)
+{long long b;
+cin>>b;
+long long store=max(6LL,b+1);
+long long calc=store/2*5;
+cout<<calc<<endl;
+}
+
+
+
+
+
+
+    return 0;
+}
