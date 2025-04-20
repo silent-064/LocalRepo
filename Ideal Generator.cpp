1e8e38fd83db0da11726790802094397de7c60ef
silent-064 <prappoprotic@gmail.com>
2025-04-20 20:31:40 +0600

    800

---
+++ Ideal Generator.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date :
+ */
+#include <bits/stdc++.h>
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ft float
+#define du double
+#define ull unsigned long long
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, i;
+    cin >> a;
+    while (a--) {
+        int b;
+        cin >> b;
+        if (b % 2 == 0) {
+            no;
+        } else {
+            yes;
+        }
+    }
+
+    return 0;
+}
