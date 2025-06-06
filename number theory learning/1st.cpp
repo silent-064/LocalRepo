6f75cce444416628b9586584b5a6d27c1fb784dc
Protic Prappo Durjoy <prappoprotic@gmail.com>
2025-06-06 13:30:05 +0600

    lesson

---
+++ number theory learning/1st.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date :
+ */
+#include <bits/stdc++.h>
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ft float
+#define du double
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+const int N = 1e5 + 10;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout << INT_MAX << endl;
+    int a = (1LL << 31) - 1;
+    cout << a << endl;
+    unsigned int b = (1LL << 32) - 1;
+    cout << b << endl;
+}
