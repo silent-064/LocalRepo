e984f7d4e8a6154ce2af161f294ba2ed0129803a
Protic Prappo Durjoy <prappoprotic@gmail.com>
2025-08-06 23:46:15 +0600

    900

--- programming c++/A_Dual_Trigger.cpp
+++ programming c++/A_Dual_Trigger.cpp
@@ -11,12 +11,33 @@
 #define ll long long
 #define ull unsigned long long
 using namespace std;
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
     int a, i;
     cin >> a;
     while (a--) {
+        ll b;
+        cin >> b;
+        string s1;
+        cin >> s1;
+
+        ll cnt = 0;
+        ll x = 0;
+
+        for (i = 0; i < b; i++) {
+            if (s1[i] == '1') {
+                cnt++;
+                x = i;
+            }
+        }
+
+        if ((cnt % 2 != 0) || (cnt == 2 && s1[x] == s1[x - 1]))
+            no;
+        else
+            yes;
     }
 }
