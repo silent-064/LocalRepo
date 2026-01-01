4ecc0cdb3ceaf28202baef26bdc7f8edb28ea771
Protic Prappo Durjoy <prappoprotic@gmail.com>
2026-01-01 13:44:55 +0600

    training

---
+++ programming c++/learningnumbertheory/isprime.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author : Protic Prappo Durjoy
+ * Date :
+ */
+#include <bits/stdc++.h>
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ft float
+#define du double
+#define ull unsigned long long
+#define ll long long
+const ll INF = 1e9 + 7;
+const ll mod = 998244353;
+using namespace std;
+bool is_prime(ll n){
+    ll i;
+    for(i=2; i*i<=n; i++){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, i;
+    cin >> a;
+    while (a--)
+    {
+
+ll n;
+cin>>n;
+cout<<is_prime(n)<<endl;
+
+
+
+    }
+}
